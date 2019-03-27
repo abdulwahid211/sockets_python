1f02a353b25a9799a32bef56cc3db0bcb2fe00c7
abzy211 <abdulwahid211@gmail.com>
2019-03-27 16:46:50 +0000

    fixing

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ from message import Message
 
 @app.route('/')
 def index():
+    port = 8000
+    host = "0.0.0.0"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host, port))
+    s.listen(100)
+    print("Server Socket successfully created")
     connection, addr = s.accept()
     print('Connected to :', addr[0], ':', addr[1])
 
 
 if __name__ == '__main__':
-    port = 8000
-    host = "0.0.0.0"
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     app.run()
-    s.bind((host, port))
-    s.listen(100)
-    print("Server Socket successfully created")
