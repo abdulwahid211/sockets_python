afcf2654cc5d5858edc82edd4b2a66a1510d73f1
abzy211 <abdulwahid211@gmail.com>
2019-03-27 16:17:33 +0000

    fixing

--- server.py
+++ server.py
@@ -2,17 +2,20 @@ import socket
 import _thread
 from message import Message
 import pickle
+import os
+from flask import Flask
+app = Flask(__name__)
 
+if __name__ == "__main__":
+port = int(os.environ.get("PORT", 5000))
+host = "0.0.0.0"
+app.run(host=host, port=port)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-port = 12345
-host = "0.0.0.0"
 s.bind((host, port))
 s.listen(100)
 list_of_clients = []
 print("Server Socket successfully created")
-
-
 m = Message("Server", "Welcome to the chatroom mate!")
 
 
@@ -68,20 +71,20 @@ def remove(connect):
     if connect in list_of_clients:
         list_of_clients.remove(connect)
 
+@app.route("/")
+def run():
+    # a forever loop until client wants to exit
+    while True:
+        connection, addr = s.accept()
 
-# a forever loop until client wants to exit
-while True:
-    connection, addr = s.accept()
+        print('Connected to :', addr[0], ':', addr[1])
 
-    print('Connected to :', addr[0], ':', addr[1])
+        """Maintains a list of clients for ease of broadcasting 
+           a message to all available people in the chatroom"""
+        list_of_clients.append(connection)
 
-    """Maintains a list of clients for ease of broadcasting 
-       a message to all available people in the chatroom"""
-    list_of_clients.append(connection)
+        # Start a new thread and return its identifier
+        _thread.start_new_thread(process, (connection,))
 
-    # Start a new thread and return its identifier
-    _thread.start_new_thread(process, (connection,))
+    s.close()
 
-s.close()
-
-# https://www.geeksforgeeks.org/simple-chat-room-using-python/
