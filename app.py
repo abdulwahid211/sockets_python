f4e2402999eb2b0ac85963d269864fa34f933324
abzy211 <abdulwahid211@gmail.com>
2019-03-27 16:41:31 +0000

    fixing

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 # app.py
-"""Flask App Project."""
-
 from flask import Flask, jsonify
 app = Flask(__name__)
+import socket
+import _thread
+from message import Message
+
 
 
 @app.route('/')
 def index():
-    """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
+    connection, addr = s.accept()
+    print('Connected to :', addr[0], ':', addr[1])
 
 
 if __name__ == '__main__':
+    port = 8000
+    host = "0.0.0.0"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     app.run()
+    s.bind((host, port))
+    s.listen(100)
+    print("Server Socket successfully created")
